a5fc41c74d04bf8a039aa0553b6abe4b127799ee
AdamSzklarz <56762102+AdamSzklarz@users.noreply.github.com>
2021-01-06 23:57:35 +0100

    Update create-user.sql

--- sql-scripts/create-user.sql
+++ sql-scripts/create-user.sql
@@ -1,2 +1,3 @@
 CREATE USER 'rentaluser'@'localhost' IDENTIFIED BY 'rentaluser';
 GRANT ALL PRIVILEGES ON * . * TO 'rentaluser'@'localhost';
+ALTER USER 'rentaluser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rentaluser';
